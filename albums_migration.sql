68cfc29a9f66e4bca66468dae9839a243967666b
Stephen Apolinar <stephenapolinar@Stephens-MacBook-Pro.local>
2018-08-10 15:06:07 -0500

    tables

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,2 +1,18 @@
-SELECT user, host From mysql.user;
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
 
+
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) DEFAULT 'NONE',
+    record_name  VARCHAR(100) NOT NULL,
+    release_date DATE NOT NULL,
+    sales INT UNSIGNED,
+    genre VARCHAR(50) DEFAULT 'NONE',
+    PRIMARY KEY (id)
+
+);
+
+SHOW TABLES;
+DESCRIBE albums;
+SHOW CREATE TABLE albums\G
